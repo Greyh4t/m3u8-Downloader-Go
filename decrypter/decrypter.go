44ac42f18598f10fec50f5499201786dd36361af
Greyh4t <Greyh4t@users.noreply.github.com>
2021-07-14 18:30:37 +0800

    Update decrypter.go
    rename padding name

--- decrypter/decrypter.go
+++ decrypter/decrypter.go
@@ -14,10 +14,10 @@ func Decrypt(data, key, iv []byte) ([]byte, error) {
 	cbc := cipher.NewCBCDecrypter(block, iv)
 	cbc.CryptBlocks(data, data)
 
-	return zeroUnPadding(data), nil
+	return PKCS7UnPadding(data), nil
 }
 
-func zeroUnPadding(origData []byte) []byte {
+func PKCS7UnPadding(origData []byte) []byte {
 	length := len(origData)
 	unpadding := int(origData[length-1])
 	return origData[:(length - unpadding)]
